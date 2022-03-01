20a8b01181d5f2b6e20c346b9653593721d3f77c
Nick Broglio <nickrbroglio@gmail.com>
2022-03-01 14:36:04 -0500

    added function for video and search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
-fetch("https://pokeapi.co/api/v2/")
+let searchInput = document.querySelector("#search-text");
+let button = document.querySelector(".button")
+
+
+fetch("https://pokeapi.co/api/v2/pokemon/")
 .then(headers => headers.json())
 .then(response => {
     console.log(response)
-})
+});
 
-// elden%20ring%20trailer
-
-let search = ""
-search.replace(/\s/g, "")
 function getVideo(){
+    // get value of search box
+    let search = searchInput.value;
+    console.log(search);
+    // update search variable to be value of search box
+    search.trim()
+    search.replace(/\s/g, "")
     let url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q="+search+"&key=AIzaSyAlM-fUBenyXh7wZecnf-yQYNTOi8mrHNM"
     fetch(url)
     .then(response => response.json())
@@ -17,4 +23,6 @@ function getVideo(){
         console.log(singleVideoID)
     })
 }
-getVideo();
+// getVideo();
+// add onclick listener to search button that runs the get video function
+button.addEventListener("click", getVideo);
