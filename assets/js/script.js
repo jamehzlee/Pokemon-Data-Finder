9078ff3c1722c669b9b49c73c5863641b4a94412
Nick Broglio <nickrbroglio@gmail.com>
2022-03-01 11:59:39 -0500

    youtube api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,36 @@
-fetch("https://store.steampowered.com/api/appdetails?appids=440")
-.then(headers => headers.json())
-.then(response => {
-    console.log(response)
-})
+// fetch("https://store.steampowered.com/api/appdetails?appids=440")
+// .then(headers => headers.json())
+// .then(response => {
+//     console.log(response)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// elden%20ring%20trailer&key
+
+
+let search = "GTA V trailer"
+search.replace(/\s/g, '')
+function getVideo(){
+    let url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q="+search+"=AIzaSyAlM-fUBenyXh7wZecnf-yQYNTOi8mrHNM"
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let singleVideoID = data.items[1].id.videoId
+        console.log(singleVideoID)
+    })
+    
+    
+}
+getVideo();
