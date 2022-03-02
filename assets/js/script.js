a7e11069005fa91f08312bf844a584a02b73e595
James Lee <probableeme@gmail.com>
2022-03-02 11:13:56 -0500

    displays first video for searched pokemon

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,18 @@ let searchInput = document.querySelector("#search-text");
 let button = document.querySelector(".button")
 
 function getData() {
-    let search = searchInput.value;
-    console.log(search)
+    let search = searchInput.value
+    let search2 = search.toLowerCase()
+    
+    console.log(search2)
     fetch("https://pokeapi.co/api/v2/pokemon/"+search+"/")
     .then(headers => headers.json())
     .then(response => {
         console.log(response)
-        let currentPokemon = response.abilities[0].ability.name
-        console.log(currentPokemon)
-        let currentPokemon1 = response.abilities[1].ability.name
-        console.log(currentPokemon1)
+        let ability1 = response.abilities[0].ability.name
+        console.log(ability1)
+        let ability2 = response.abilities[1].ability.name
+        console.log(ability2)
     });
 }
 
@@ -21,12 +23,17 @@ function getVideo(){
     // update search variable to be value of search box
     search.trim()
     search.replace(/\s/g, "")
-    let url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q="+search+"&key=AIzaSyAwNZ2kW0ApwqX9uW-ZJnuNgQkXePsnjCw"
-    fetch(url)
+    let videoInfo = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q="+search+"&key=AIzaSyAwNZ2kW0ApwqX9uW-ZJnuNgQkXePsnjCw"
+    fetch(videoInfo)
     .then(response => response.json())
     .then(data => {
-        let singleVideoID = data.items[1].id.videoId
-        console.log(singleVideoID)
+        let videoId = data.items[0].id.videoId
+        let url = "https://youtube.com/embed/"+videoId
+        let iframe = document.querySelector("iframe")
+        console.log(data)
+        iframe.setAttribute("src", url)
+        console.log(iframe)
+
     })
 }
 
